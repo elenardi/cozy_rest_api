dcba2db5b961df5da2d9402f21b2224eca650413
Elen Ardiyanto <elard.yanto@gmail.com>
2022-12-27 16:02:00 +0700

    set freeDB.net

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const dotenv = require ('dotenv')
 dotenv.config()
 
 module.exports = {
-    HOST: "localhost",
+    HOST: process.env.DB_HOST,
     USER: process.env.DB_USERNAME,
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME,
